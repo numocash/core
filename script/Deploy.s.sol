93770a9e0777aae04f3493661f200de431acc717
Kyle Scott <kyscott@umich.edu>
2022-10-03 20:41:40 -0400

    deploy scripts

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -4,16 +4,11 @@ pragma solidity ^0.8.13;
 import "forge-std/Script.sol";
 import "forge-std/console2.sol";
 import { Factory } from "../src/Factory.sol";
-import { Lendgine } from "../src/Lendgine.sol";
-import { Position } from "../src/libraries/Position.sol";
-
-import { LendgineAddress } from "../src/libraries/LendgineAddress.sol";
 
 contract DeployScript is Script {
     function run() public {
         uint256 pk = vm.envUint("PRIVATE_KEY");
         vm.broadcast(pk);
         new Factory();
-        vm.broadcast(pk);
     }
 }
