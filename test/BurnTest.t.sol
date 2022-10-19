5b73de48590801b82d84ec63be7a32cb017addd6
Kyle Scott <kyscott@umich.edu>
2022-10-19 17:06:16 -0400

    basic mint and burn tests

--- test/BurnTest.t.sol
+++ test/BurnTest.t.sol
@@ -1,91 +1,74 @@
-// pragma solidity ^0.8.4;
+pragma solidity ^0.8.4;
 
-// import "forge-std/console2.sol";
+import "forge-std/console2.sol";
 
-// import { TestHelper } from "./utils/TestHelper.sol";
-// import { CallbackHelper } from "./utils/CallbackHelper.sol";
+import { TestHelper } from "./utils/TestHelper.sol";
+import { CallbackHelper } from "./utils/CallbackHelper.sol";
 
-// import { Position } from "../src/libraries/Position.sol";
-// import { Tick } from "../src/libraries/Tick.sol";
+import { Position } from "../src/libraries/Position.sol";
 
-// import { Factory } from "../src/Factory.sol";
-// import { Lendgine } from "../src/Lendgine.sol";
+import { Factory } from "../src/Factory.sol";
+import { Lendgine } from "../src/Lendgine.sol";
 
-// contract BurnTest is TestHelper {
-//     bytes32 public positionID;
+contract BurnTest is TestHelper {
+    function setUp() public {
+        _setUp();
 
-//     function setUp() public {
-//         _setUp();
+        _deposit(1 ether, 8 ether, 1 ether, cuh);
 
-//         _deposit(1 ether, 8 ether, 1 ether, 1, cuh);
+        _mint(10 ether, cuh);
+    }
 
-//         _mint(10 ether, cuh);
+    function testBurnPartial() public {
+        _burn(0.5 ether, cuh);
 
-//         positionID = Position.getID(cuh, 1);
-//     }
+        // Test lendgine token
+        assertEq(lendgine.totalSupply(), 0.5 ether);
+        assertEq(lendgine.balanceOf(cuh), 0.5 ether);
+        assertEq(lendgine.balanceOf(address(lendgine)), 0 ether);
 
-//     function testBurnPartial() public {
-//         _burn(0.5 ether, cuh);
+        // // Test base token
+        assertEq(pair.buffer(), 0.5 ether);
 
-//         // Test lendgine token
-//         assertEq(lendgine.totalSupply(), 0.5 ether);
-//         assertEq(lendgine.balanceOf(cuh), 0.5 ether);
-//         assertEq(lendgine.balanceOf(address(lendgine)), 0 ether);
+        // Test speculative token
+        assertEq(speculative.balanceOf(cuh), 5 ether);
+        assertEq(speculative.balanceOf(address(lendgine)), 5 ether);
 
-//         // // Test base token
-//         assertEq(pair.buffer(), 0.5 ether);
+        assertPosition(Position.Info({ liquidity: 1 ether, rewardPerLiquidityPaid: 0, tokensOwed: 0 }), cuh);
 
-//         // Test speculative token
-//         assertEq(speculative.balanceOf(cuh), 5 ether);
-//         assertEq(speculative.balanceOf(address(lendgine)), 5 ether);
+        // Test global storage values
+        assertEq(lendgine.totalLiquidity(), 1 ether);
+        assertEq(lendgine.totalLiquidityBorrowed(), 0.5 ether);
+        assertEq(lendgine.rewardPerLiquidityStored(), 0);
+        assertEq(lendgine.lastUpdate(), 1);
+    }
 
-//         assertPosition(Position.Info({ liquidity: 1 ether, rewardPerLiquidityPaid: 0, tokensOwed: 0 }), positionID);
+    function testBurnFull() public {
+        _burn(1 ether, cuh);
 
-//         assertTick(
-//             Tick.Info({ liquidity: 1 ether, rewardPerINPaid: 0, tokensOwedPerLiquidity: 0, prev: 0, next: 0 }),
-//             1
-//         );
+        // Test lendgine token
+        assertEq(lendgine.totalSupply(), 0 ether);
+        assertEq(lendgine.balanceOf(cuh), 0 ether);
+        assertEq(lendgine.balanceOf(address(lendgine)), 0 ether);
 
-//         // Test global storage values
-//         assertEq(lendgine.currentTick(), 1);
-//         assertEq(lendgine.currentLiquidity(), 0.5 ether);
-//         assertEq(lendgine.rewardPerINStored(), 0);
-//         assertEq(lendgine.lastUpdate(), 1);
-//         assertEq(lendgine.interestNumerator(), 0.5 ether);
-//     }
+        // Test pair token
+        assertEq(pair.buffer(), 0 ether);
 
-//     function testBurnFull() public {
-//         _burn(1 ether, cuh);
+        // Test speculative token
+        assertEq(speculative.balanceOf(cuh), 10 ether);
+        assertEq(speculative.balanceOf(address(lendgine)), 0);
 
-//         // Test lendgine token
-//         assertEq(lendgine.totalSupply(), 0 ether);
-//         assertEq(lendgine.balanceOf(cuh), 0 ether);
-//         assertEq(lendgine.balanceOf(address(lendgine)), 0 ether);
+        assertPosition(Position.Info({ liquidity: 1 ether, rewardPerLiquidityPaid: 0, tokensOwed: 0 }), cuh);
 
-//         // Test pair token
-//         assertEq(pair.buffer(), 0 ether);
+        // Test global storage values
+        assertEq(lendgine.totalLiquidity(), 1 ether);
+        assertEq(lendgine.totalLiquidityBorrowed(), 0 ether);
+        assertEq(lendgine.rewardPerLiquidityStored(), 0);
+        assertEq(lendgine.lastUpdate(), 1);
+    }
 
-//         // Test speculative token
-//         assertEq(speculative.balanceOf(cuh), 10 ether);
-//         assertEq(speculative.balanceOf(address(lendgine)), 0);
-
-//         assertPosition(Position.Info({ liquidity: 1 ether, rewardPerLiquidityPaid: 0, tokensOwed: 0 }), positionID);
-
-//         assertTick(
-//             Tick.Info({ liquidity: 1 ether, rewardPerINPaid: 0, tokensOwedPerLiquidity: 0, prev: 0, next: 0 }),
-//             1
-//         );
-
-//         // Test global storage values
-//         assertEq(lendgine.currentTick(), 1);
-//         assertEq(lendgine.currentLiquidity(), 0);
-//         assertEq(lendgine.rewardPerINStored(), 0);
-//         assertEq(lendgine.lastUpdate(), 1);
-//         assertEq(lendgine.interestNumerator(), 0);
-//     }
-
-//     function testZeroBurn() public {
-//         vm.expectRevert(Lendgine.InsufficientOutputError.selector);
-//         lendgine.burn(cuh);
-//     }
-// }
+    function testZeroBurn() public {
+        vm.expectRevert(Lendgine.InsufficientOutputError.selector);
+        lendgine.burn(cuh);
+    }
+}
--- test/MintTest.t.sol
+++ test/MintTest.t.sol
@@ -1,126 +1,107 @@
-// pragma solidity ^0.8.4;
+pragma solidity ^0.8.4;
 
-// import "forge-std/console2.sol";
+import "forge-std/console2.sol";
 
-// import { TestHelper } from "./utils/TestHelper.sol";
+import { TestHelper } from "./utils/TestHelper.sol";
 
-// import { Position } from "../src/libraries/Position.sol";
-// import { Tick } from "../src/libraries/Tick.sol";
+import { Position } from "../src/libraries/Position.sol";
 
-// import { Factory } from "../src/Factory.sol";
-// import { Lendgine } from "../src/Lendgine.sol";
-// import { CallbackHelper } from "./utils/CallbackHelper.sol";
+import { Factory } from "../src/Factory.sol";
+import { Lendgine } from "../src/Lendgine.sol";
+import { CallbackHelper } from "./utils/CallbackHelper.sol";
 
-// contract MintTest is TestHelper {
-//     bytes32 public positionID;
+contract MintTest is TestHelper {
+    bytes32 public positionID;
 
-//     function setUp() public {
-//         _setUp();
+    function setUp() public {
+        _setUp();
 
-//         _deposit(1 ether, 8 ether, 1 ether, 1, cuh);
+        _deposit(1 ether, 8 ether, 1 ether, cuh);
+    }
 
-//         positionID = Position.getID(cuh, 1);
-//     }
+    function testMint() public {
+        _mint(5 ether, cuh);
 
-//     function testMint() public {
-//         _mint(5 ether, cuh);
-//         // console2.log(pair.calcInvariant(10 ether, 0 ether));
+        // Test lendgine token
+        assertEq(lendgine.totalSupply(), 0.5 ether);
+        assertEq(lendgine.balanceOf(cuh), 0.5 ether);
+        assertEq(lendgine.balanceOf(address(lendgine)), 0 ether);
 
-//         // Test lendgine token
-//         assertEq(lendgine.totalSupply(), 0.5 ether);
-//         assertEq(lendgine.balanceOf(cuh), 0.5 ether);
-//         assertEq(lendgine.balanceOf(address(lendgine)), 0 ether);
+        assertPosition(Position.Info({ liquidity: 1 ether, rewardPerLiquidityPaid: 0, tokensOwed: 0 }), cuh);
 
-//         // Test pair token
-//         assertEq(pair.buffer(), 0.5 ether);
-//         assertEq(pair.totalSupply(), 1 ether);
+        // Test global storage values
+        assertEq(lendgine.totalLiquidity(), 1 ether);
+        assertEq(lendgine.totalLiquidityBorrowed(), 0.5 ether);
+        assertEq(lendgine.rewardPerLiquidityStored(), 0);
+        assertEq(lendgine.lastUpdate(), 1);
 
-//         assertPosition(Position.Info({ liquidity: 1 ether, rewardPerLiquidityPaid: 0, tokensOwed: 0 }), positionID);
+        // Test pair token
+        assertEq(pair.buffer(), 0.5 ether);
+        assertEq(pair.totalSupply(), 1 ether);
+    }
 
-//         assertTick(
-//             Tick.Info({ liquidity: 1 ether, rewardPerINPaid: 0, tokensOwedPerLiquidity: 0, prev: 0, next: 0 }),
-//             1
-//         );
+    function testZeroMint() public {
+        vm.expectRevert(Lendgine.InsufficientOutputError.selector);
+        lendgine.mint(
+            cuh,
+            0 ether,
+            abi.encode(CallbackHelper.CallbackData({ speculative: address(speculative), payer: cuh }))
+        );
+    }
 
-//         // Test global storage values
-//         assertEq(lendgine.currentTick(), 1);
-//         assertEq(lendgine.currentLiquidity(), 0.5 ether);
-//         assertEq(lendgine.rewardPerINStored(), 0);
-//         assertEq(lendgine.lastUpdate(), 1);
-//         assertEq(lendgine.interestNumerator(), 0.5 ether);
-//     }
+    // TODO: test donations to the pool and extra mints
+    // TODO: test insufficient inputs in a different file
 
-//     // function testInsufficientInput() public {
-//     //     speculative.mint(cuh, 1 ether);
+    function testExtraMint() public {
+        uint256 amountS = 10 ether + 10;
 
-//     //     vm.prank(cuh);
-//     //     speculative.approve(address(this), 1 ether);
+        speculative.mint(cuh, amountS);
 
-//     //     vm.expectRevert(Lendgine.InsufficientInputError.selector);
+        vm.prank(cuh);
+        speculative.approve(address(this), amountS);
 
-//     //     lendgine.mint(
-//     //         address(this),
-//     //         2 ether,
-//     //         abi.encode(MintCallbackHelper.MintCallbackData({ key: key, payer: address(this) }))
-//     //     );
-//     // }
+        vm.expectRevert(Lendgine.CompleteUtilizationError.selector);
+        lendgine.mint(
+            cuh,
+            amountS,
+            abi.encode(CallbackHelper.CallbackData({ speculative: address(speculative), payer: cuh }))
+        );
+    }
 
-//     function testZeroMint() public {
-//         vm.expectRevert(Lendgine.InsufficientOutputError.selector);
-//         lendgine.mint(
-//             cuh,
-//             0 ether,
-//             abi.encode(CallbackHelper.CallbackData({ speculative: address(speculative), payer: cuh }))
-//         );
-//     }
+    function testEmptyMint() public {
+        _withdraw(1 ether, cuh);
 
-//     // function testExtraMint() public {
-//     //     speculative.mint(cuh, 21 ether);
+        speculative.mint(cuh, 1 ether);
 
-//     //     vm.prank(cuh);
-//     //     speculative.approve(address(this), 21 ether);
+        vm.prank(cuh);
+        speculative.approve(address(this), 1 ether);
 
-//     //     vm.expectRevert(Lendgine.CompleteUtilizationError.selector);
-//     //     lendgine.mint(cuh, 21 ether, abi.encode(CallbackHelper.CallbackData({ key: key, payer: cuh })));
-//     // }
+        vm.expectRevert(Lendgine.CompleteUtilizationError.selector);
+        lendgine.mint(
+            cuh,
+            1 ether,
+            abi.encode(CallbackHelper.CallbackData({ speculative: address(speculative), payer: cuh }))
+        );
+    }
 
-//     // function testEmptyMint() public {
-//     //     _withdraw(2 ether - 1000, cuh);
+    function testMintFull() public {
+        _mint(10 ether, cuh);
 
-//     //     speculative.mint(cuh, 1 ether);
+        // Test lendgine token
+        assertEq(lendgine.totalSupply(), 1 ether);
+        assertEq(lendgine.balanceOf(cuh), 1 ether);
+        assertEq(lendgine.balanceOf(address(lendgine)), 0 ether);
 
-//     //     vm.prank(cuh);
-//     //     speculative.approve(address(this), 1 ether);
+        assertPosition(Position.Info({ liquidity: 1 ether, rewardPerLiquidityPaid: 0, tokensOwed: 0 }), cuh);
 
-//     //     vm.expectRevert(Lendgine.CompleteUtilizationError.selector);
-//     //     lendgine.mint(cuh, 1 ether, abi.encode(CallbackHelper.CallbackData({ key: key, payer: cuh })));
-//     // }
+        // Test global storage values
+        assertEq(lendgine.totalLiquidity(), 1 ether);
+        assertEq(lendgine.totalLiquidityBorrowed(), 1 ether);
+        assertEq(lendgine.rewardPerLiquidityStored(), 0);
+        assertEq(lendgine.lastUpdate(), 1);
 
-//     function testMintFull() public {
-//         _mint(10 ether, cuh);
-//         // console2.log(pair.calcInvariant(10 ether, 0 ether));
-
-//         // Test lendgine token
-//         assertEq(lendgine.totalSupply(), 1 ether);
-//         assertEq(lendgine.balanceOf(cuh), 1 ether);
-//         assertEq(lendgine.balanceOf(address(lendgine)), 0 ether);
-
-//         // Test pair token
-//         assertEq(pair.buffer(), 1 ether);
-//         assertEq(pair.totalSupply(), 1 ether);
-
-//         assertPosition(Position.Info({ liquidity: 1 ether, rewardPerLiquidityPaid: 0, tokensOwed: 0 }), positionID);
-
-//         assertTick(
-//             Tick.Info({ liquidity: 1 ether, rewardPerINPaid: 0, tokensOwedPerLiquidity: 0, prev: 0, next: 0 }),
-//             1
-//         );
-
-//         // Test global storage values
-//         assertEq(lendgine.currentTick(), 1);
-//         assertEq(lendgine.currentLiquidity(), 1 ether);
-//         assertEq(lendgine.rewardPerINStored(), 0);
-//         assertEq(lendgine.lastUpdate(), 1);
-//         assertEq(lendgine.interestNumerator(), 1 ether);
-//     }
-// }
+        // Test pair token
+        assertEq(pair.buffer(), 1 ether);
+        assertEq(pair.totalSupply(), 1 ether);
+    }
+}
